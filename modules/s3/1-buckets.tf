2ed81e7834100bbc1debc7a400441ae3a2fe9bb8
gaava <nero.gava@gmail.com>
2025-02-02 16:03:14 -0300

    fix: cognito and s3

--- modules/s3/1-buckets.tf
+++ modules/s3/1-buckets.tf
@@ -7,15 +7,39 @@ resource "aws_s3_bucket" "bucket" {
   }
 }
 
+resource "aws_s3_bucket_public_access_block" "bucket" {
+  bucket = aws_s3_bucket.bucket.id
+
+  block_public_acls       = true   # Bloqueia ACLs públicas (recomendado)
+  block_public_policy     = false  # Permite políticas públicas (necessário!)
+  ignore_public_acls      = true   # Ignora ACLs públicas (recomendado)
+  restrict_public_buckets = false  # Permite políticas públicas específicas
+}
+
+resource "aws_s3_bucket_policy" "public_policy" {
+  bucket = aws_s3_bucket.bucket.id
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "PublicReadForSpecificFolder"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "arn:aws:s3:::${aws_s3_bucket.bucket.bucket}/output_zip/*"
+      }
+    ]
+  })
+}
+
 resource "aws_s3_object" "videos_folder" {
-  bucket = aws_s3_bucket.bucket.bucket
+  bucket = aws_s3_bucket.bucket.id
   key    = "videos_input/" 
-  acl    = "private"
+  content_type = "application/x-directory"
 }
 
-resource "aws_s3_object" "folder2" {
-  bucket = aws_s3_bucket.bucket.bucket
+resource "aws_s3_object" "output_zip_folder" {
+  bucket = aws_s3_bucket.bucket.id
   key    = "output_zip/" 
-  acl    = "private"
+  content_type = "application/x-directory"
 }
-
